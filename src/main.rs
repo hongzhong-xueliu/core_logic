e3be25758823e3c57f13f1a12271184c7aa85b52
hsjoihs / sozysozbot <sozysozbot@gmail.com>
2024-07-18 03:36:14 +0900

    手牌交換を実装

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use core_logic::牌;
-use itertools::Itertools;
 use rand::seq::SliceRandom;
-use rand::thread_rng;
+use rand::{thread_rng, Rng};
 
 /// 「全部俺」モード
 fn main() {
@@ -22,11 +21,75 @@ fn main() {
     手牌s[2].sort();
     手牌s[3].sort();
 
-    // TODO: 手牌交換
+    println!("##########################################################");
+    println!("###  あなたは「全部俺」モードでプレイしています。");
+    println!("##########################################################");
 
-    // 欠色選択
+    println!("\n\n--------------------------------------------------");
+    println!("|  手牌交換");
+    println!("--------------------------------------------------");
+
+    let mut 交換に出される牌s = [Vec::new(), Vec::new(), Vec::new(), Vec::new()];
+
+    for (i, 手牌) in 手牌s.iter_mut().enumerate() {
+        println!("\n\n========= ↓ 以下、プレイヤー{i}の選択 ↓ =========================");
+        loop {
+            print!("プレイヤー{i}の手牌: [");
+            for (j, 牌) in 手牌.iter().enumerate() {
+                if j != 0 {
+                    print!(", ");
+                }
+                print!("{j}: {牌:?}");
+            }
+            println!("]");
+            println!("交換に出す牌: {:?}", 交換に出される牌s[i]);
+            println!(
+                "交換する牌の index を指定してください。 #{}",
+                交換に出される牌s[i].len() + 1
+            );
+            let index = loop {
+                let mut index = String::new();
+                std::io::stdin().read_line(&mut index).unwrap();
+                let index: usize = index.trim().parse().unwrap();
+                if index >= 手牌.len() {
+                    println!("index out of bounds");
+                    continue;
+                }
+                break index;
+            };
+            let 交換する牌 = 手牌.remove(index);
+            交換に出される牌s[i].push(交換する牌);
+
+            if 交換に出される牌s[i].len() == 3 {
+                break;
+            }
+        }
+    }
+
+    let 交換先offset = rng.gen_range(1..=3);
+
+    println!("\n\n--------------------------------------------------");
+    println!(
+        "|  この卓では、3 枚が{}へ送られます。",
+        ["", "下家", "対面", "上家"][交換先offset]
+    );
+    println!("--------------------------------------------------");
+
+    for (i, 送る3牌) in 交換に出される牌s.iter().enumerate() {
+        let 送る先 = (i + 交換先offset) % 4;
+        for 送る牌 in 送る3牌 {
+            手牌s[送る先].push(*送る牌);
+        }
+        手牌s[送る先].sort();
+    }
+
+    println!("\n\n--------------------------------------------------");
+    println!("|  欠色選択");
+    println!("--------------------------------------------------");
+
     let mut 欠色s: [u8; 4] = [0; 4];
     for (i, 欠色) in &mut 欠色s.iter_mut().enumerate() {
+        println!("\n\n========= ↓ 以下、プレイヤー{i}の選択 ↓ =========================");
         *欠色 = loop {
             println!("プレイヤー{i}の手牌: {:?}", 手牌s[i]);
             println!("プレイヤー{i}の欠色を入力してください。(0: 萬, 1: 筒, 2: 索)");
